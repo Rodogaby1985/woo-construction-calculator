1e392daf2f679b9f4ad7c3295479795df2bc09eb
Rodogaby1985 <rodogaby85@gmail.com>
2025-07-14 07:58:13 -0300

    Update WallCalculatorHeader.js

--- src/components/WallCalculator/WallCalculatorHeader.js
+++ src/components/WallCalculator/WallCalculatorHeader.js
@@ -3,12 +3,6 @@ import React from 'react';
 const WallCalculatorHeader = () => {
   return (
     <div className="text-center mb-8">
-      {/* Asumiendo que guardaste el logo en la carpeta /public */}
-      <img
-        src="/logo.png" /* <-- Ruta local */
-        alt="Logo Blockplas"
-        className="mx-auto mb-4 w-[200px]" /* <-- Ancho con Tailwind */
-      />
       <h1 className="text-3xl font-bold text-gray-900 mb-2">
         Calculadora Profesional de Ladrillos
       </h1>
