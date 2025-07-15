3e64edf38f51e3148b5cb542d196c0d31b82c2bb
Rodogaby1985 <rodogaby85@gmail.com>
2025-07-15 14:46:52 -0300

    Update index.js

--- src/components/WallCalculator/index.js
+++ src/components/WallCalculator/index.js
@@ -2,5 +2,8 @@ export { default as WallCalculatorForm } from './WallCalculatorForm';
 export { default as WallCalculatorResults } from './WallCalculatorResults';
 export { default as WallCalculatorHeader } from './WallCalculatorHeader';
 export { default as WallSelector } from './WallSelector';
+// Añade la exportación del nuevo componente para que App.js pueda importarlo
+export { default as TerminationSelector } from './TerminationSelector';
+
 
 // DONE
