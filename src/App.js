c770c47d86869fd52e7168e95efc8032e54e5021
Rodogaby1985 <rodogaby85@gmail.com>
2025-07-16 19:41:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import React, { useState } from 'react';
-// Añadimos el nuevo componente a la importación
-import { WallCalculatorHeader, WallCalculatorForm, WallCalculatorResults, WallSelector, TerminationSelector } from './components/WallCalculator';
+import { WallCalculatorHeader, WallCalculatorForm, WallCalculatorResults, WallSelector } from './components/WallCalculator';
 
 const App = () => {
   const [layoutType, setLayoutType] = useState('single');
-  const [walls, setWalls] = useState([{ height: '', width: '', openings: [] }]);
   
-  // Nuevo estado para manejar el número de terminaciones.
-  // Por defecto, una pared simple tiene 2.
-  const [numTerminations, setNumTerminations] = useState(2);
+  // El estado 'walls' ahora manejará todas las paredes.
+  // Cada pared tendrá un ID único y un tipo ('perimeter' o 'divider').
+  const [walls, setWalls] = useState([
+    { id: 1, type: 'perimeter', height: '', width: '', openings: [] }
+  ]);
 
+  // Se ejecuta cuando el usuario elige una forma (1 pared, L, U, etc.)
   const handleLayoutSelect = (type) => {
     setLayoutType(type);
 
     const wallCountMap = { 'single': 1, 'L': 2, 'U': 3, 'square': 4 };
-    const terminationsMap = { 'single': 2, 'L': 2, 'U': 2, 'square': 0 };
-
     const wallCount = wallCountMap[type] || 1;
-    // Establecemos un número de terminaciones por defecto según la forma.
-    const defaultTerminations = terminationsMap[type] !== undefined ? terminationsMap[type] : 2;
 
-    const newWalls = Array.from({ length: wallCount }, () => ({
+    // Crea solo las paredes perimetrales
+    const newPerimeterWalls = Array.from({ length: wallCount }, (_, i) => ({
+      id: Date.now() + i, // ID único
+      type: 'perimeter',
       height: '',
       width: '',
       openings: []
     }));
 
-    setWalls(newWalls);
-    setNumTerminations(defaultTerminations);
+    // Mantiene las paredes divisorias que ya existían
+    const existingDividers = walls.filter(wall => wall.type === 'divider');
+    setWalls([...newPerimeterWalls, ...existingDividers]);
+  };
+
+  // Función para AÑADIR una nueva pared divisoria
+  const addDividerWall = () => {
+    const newDivider = {
+      id: Date.now(),
+      type: 'divider',
+      height: '',
+      width: '',
+      openings: []
+    };
+    setWalls(prevWalls => [...prevWalls, newDivider]);
   };
 
   return (
@@ -40,16 +53,15 @@ const App = () => {
               selectedLayout={layoutType}
               onSelectLayout={handleLayoutSelect}
             />
-            {/* Añadimos el nuevo selector de terminaciones */}
-            <TerminationSelector 
-              numTerminations={numTerminations}
-              setNumTerminations={setNumTerminations}
+            {/* El formulario ahora maneja todas las paredes y la función de añadir divisorias */}
+            <WallCalculatorForm 
+              walls={walls} 
+              setWalls={setWalls}
+              addDividerWall={addDividerWall}
             />
-            <WallCalculatorForm walls={walls} setWalls={setWalls} />
           </div>
           <div className="border-t border-gray-200">
-             {/* Pasamos el número de terminaciones al componente de resultados */}
-            <WallCalculatorResults walls={walls} numTerminations={numTerminations} />
+            <WallCalculatorResults walls={walls} layoutType={layoutType} />
           </div>
         </div>
       </div>
@@ -58,4 +70,3 @@ const App = () => {
 };
 
 export default App;
-
